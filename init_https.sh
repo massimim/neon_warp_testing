58f933d50009df84d6e2bff802c275bb2fcfe51e
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2024-06-05 17:14:11 +0200

    Init script that uses https instead of ssh for cloning.

---
+++ init_https.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/massimim/neon_warp_testing.git -b py neon_py_bindings
+git clone https://github.com/massimim/warp -b external-source-support warp
+
+cd neon_py_bindings
+mkdir cmake-build-debug
+cd cmake-build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --target libNeonPy -j 30
