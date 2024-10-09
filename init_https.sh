67880c1216f8e794fd24bdb97ac446e8b6a2df16
Massimiliano Meneghin <massimiliano.meneghin@autodesk.com>
2024-10-09 19:43:47 +0200

    WIP

--- init_https.sh
+++ init_https.sh
@@ -5,7 +5,7 @@ rm -fr neon_py_bindings
 
 git clone https://github.com/Autodesk/Neon.git -b py neon_py_bindings
 git clone https://github.com/massimim/warp -b external-source-support warp_src
-git clone https://github.com/massimim/XLB.git
+git clone https://github.com/massimim/XLB.git -b XLB_src
 
 cd neon_py_bindings
 mkdir cmake-build-debug
